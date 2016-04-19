97b7b127e5a57a533bd9b7ba2c0b0ecbfa9380a7
andrearosado <andrearosado93@gmail.com>
2016-04-19 12:08:08 +0200

    Database template

---
+++ sql/database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE birthdays
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'es_ES.UTF-8'
+       LC_CTYPE = 'es_ES.UTF-8'
+       CONNECTION LIMIT = -1;
